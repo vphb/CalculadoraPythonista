1e12f844fffc40d2eb1c744eff709f4c8b709aad
vphb <victoriaphb@icloud.com>
2023-11-08 19:31:10 -0300

    Se agrega la base de interfaz grafica con lib tkinter

---
+++ interfaz_grafica.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import math
+import tkinter.messagebox
+  
+root = Tk()
+root.title("Scientific Calculator")
+root.configure(background = 'white')
+root.resizable(width=False, height=False)
+root.geometry("480x568+450+90")
+
+calc = Frame(root)
+calc.grid()
